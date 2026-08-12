c8f0cac21d257f0fd0d47183d0a192c5374710ac
RaAnFreez <rizkyrizz200830@gmail.com>
2026-08-12 23:16:11 +0800

    fix(auth): restore existing license summary module import

--- worker/src/index.js
+++ worker/src/index.js
@@ -3,7 +3,7 @@ import { login, logout, listSessions, revokeSession, forgotPassword, resetPasswo
 import { validateLicense } from "./security/license.js";
 import { updateLicenseStatus } from "./security/license-admin.js";
 import { getLicenseAudit } from "./security/license-audit.js";
-import { getUserLicenseSummary } from "./security/user-license-summary.js";
+import { getUserLicenseSummary } from "./security/license-summary.js";
 
 const SECURITY_HEADERS = {
   "content-type": "application/json; charset=utf-8",
